2fef5df44e5e84ac4566dcc8d775ce3dc8653c3c
andreypluzhnik <andreypluzhnik7@gmail.com>
2025-01-01 15:32:10 -0700

    .gitignore tweaks

---
+++ src/eval.py
@@ -0,0 +1,243 @@
+import torch
+from torch import nn
+import numpy as np
+import scipy
+import random
+import matplotlib.pyplot as plt
+from src.train import *
+import time
+
+
+def icholesky(A):
+    n = A.shape[0]
+
+    for i in range(n):
+        A[i,i] = np.sqrt(A[i,i])
+        nz, = A[i + 1:, i].nonzero()
+        if(len(nz) > 0):
+            nz = nz + (i + 1)
+            A[nz, i] = A[nz,i]/A[i,i]
+        
+        for j in nz:
+            k, = A[j:n,j].nonzero()
+            if(len(k) > 0):
+                k = k + j
+                A[k, j] = A[k, j] - A[k, i] * A[j, i]
+        
+    return np.tril(A)
+
+
+
+def next_direction(X):
+    model.eval() # set model to evaluate mode
+
+
+    with torch.no_grad():
+    #    res = res.to(DEVICE)
+        X = X.view((1, 1, dim_x, dim_y, dim_z))
+        X = model(X).squeeze().flatten()  
+        return X
+
+
+
+
+def pgd_backsolve(A:scipy.sparse.csr_matrix, R:scipy.sparse.csr_matrix,b:np.ndarray,tol = 1e-10, max_it = 3e5):
+    # solve P^T A P (iPx) = P^T b
+    # R^-1 = P * P^T
+    
+    x = np.zeros(len(b))
+    r = b.copy()
+    z = scipy.sparse.linalg.spsolve_triangular(R, r, lower = True) # stores iM * res
+    z = scipy.sparse.linalg.spsolve_triangular(R.transpose(), z, lower = False)
+    r_old = r.copy()
+    z_old = z.copy() # stores iM * res_old
+    d = z.copy()
+
+    count = 0
+    res_norm = np.linalg.norm(r)
+    res_arr = [res_norm]
+
+    while(res_norm > tol and count < max_it):
+        if(count % 2 == 0):
+            print(f"count: {count}    residual: {res_norm}")
+        # print(res_arr[-1])
+        alpha = r.dot(z)/d.dot(A.dot(d))
+        x = x + alpha * d
+        
+        r_old = r.copy()
+        z_old = z.copy()
+        r = r_old - alpha * A.dot(d) # A.dot gives a matrix-vector product in this case
+        # R backsolve
+        z = scipy.sparse.linalg.spsolve_triangular(R, r, lower = True) 
+        z = scipy.sparse.linalg.spsolve_triangular(R.transpose(), z, lower = False)
+
+        beta = r.dot(z) / r_old.dot(z_old)
+        d = z + beta * d
+        res_norm = scipy.linalg.norm(r, 2)
+        res_arr = res_arr + [res_norm]
+        
+        count+=1
+
+    print(f"Finished in {count} iterations with residual {res_arr[-1]}")
+    return res_arr, x, count
+
+
+
+def neural_pgd(A, b:torch.Tensor, tol = 1e-16, max_it = 100):
+    x = torch.zeros(b.shape[0]).to(DEVICE)
+    dk = torch.zeros(x.shape[0]).to(DEVICE)
+    dkk = torch.zeros(x.shape[0]).to(DEVICE)
+    ak = 1
+    akk = 1
+   
+    d = torch.zeros(x.shape[0])
+    res = b.clone()
+    res_norm = torch.linalg.norm(res).item()
+    # res_old = res.clone()
+    res_arr = [res_norm]
+    count = 0
+
+    while(res_norm > tol and count < max_it):
+        if(count % 2 == 0):
+            print(f"count: {count}    residual: {res_norm}")
+        d = next_direction(torch.div(res, res_norm))
+        d_temp = d.clone()
+        d = torch.sub(input = d, alpha = 1, other = torch.dot(d_temp, torch.mv(A, dk))/ak * dk)
+        d = torch.sub(input = d, alpha = 1, other = torch.dot(d_temp, torch.mv(A, dkk))/akk * dkk)
+        # d -= dk * torch.dot(d, torch.mv(A, dk))/ak - dkk * torch.dot(d, torch.mv(A, dkk))/akk
+
+        akk = ak
+        ak = torch.dot(d, torch.mv(A, d))
+        alpha = torch.dot(res, d)/ak
+        # x = x + alpha * d
+        x = torch.add(input = x, alpha = alpha, other = d)
+
+        dkk = dk.clone()
+        dk = d.clone()
+
+        res = torch.sub(input = b, alpha = 1, other = torch.mv(A, x))
+        res_norm = torch.norm(res,2) 
+        res_arr = res_arr + [res_norm.item()]
+        count += 1
+    
+    print(f"Finished in {count} iterations with residual {res_arr[-1]}")
+    return res_arr, x, count
+
+
+if __name__ == "__main__":
+    dim_x, dim_y, dim_z = 8, 8, 8
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    torch.set_default_dtype(torch.double)
+
+    domain_type = "random"
+    filling_fraction = 0.35
+    
+    fn_map = {"cube" : {"data suffix" : "cube", 
+                        "plot title":"PCG vs Neural Preconditioner for 8x8x8 Domain with Cube",
+                        "save name":"cube_plot.png"},
+                        
+            "sphere" : {"data suffix" : "sphere", 
+                        "plot title":"PCG vs Neural Preconditioner for 8x8x8 Domain with Sphere",
+                        "save name":"sphere_plot.png"},
+
+            "empty" : {"data suffix" : "", 
+                        "plot title":"PCG vs Neural Preconditioner for Empty 8x8x8 Domain",
+                        "save name":"empty_plot.png"},
+            
+            "torus" : {"data suffix" : "torus", 
+                "plot title":"PCG vs Neural Preconditioner for 8x8x8 Domain with Torus",
+                "save name":"torus_plot.png"},
+            
+            "random" : {"data suffix" : f"random_ff_{filling_fraction}", 
+                "plot title":f"PCG vs Neural Preconditioner for 8x8x8 Domain with {filling_fraction * 100}% Random Fill",
+                "save name":f"random_{filling_fraction}_plot.png"},
+            }
+
+
+    data_suffix = fn_map[domain_type]["data suffix"]
+    plot_title = fn_map[domain_type]["plot title"]
+    save_fn = fn_map[domain_type]["save name"]
+
+    models_dir = f"./models/"
+    model_fn = "8_8_8_grid_state_16_mod.pth"
+    
+        
+    model = CNN(dim_x, dim_y, dim_z).to(DEVICE)
+    model.load_state_dict(torch.load(models_dir + model_fn))
+    model.eval()
+    matrix_fn = f"A_matrix_8_8_8_{data_suffix}"
+    # load A matrix, its stored as COO
+    A = scipy.sparse.load_npz(f"./data/{matrix_fn}.npz")
+    
+    # generate a random test vector
+    vectors = np.load(f"./data/{matrix_fn}_data.npy")
+    idx = random.randint(0,vectors.shape[0] - 1)
+    b = vectors[idx,:]
+    # find solution to desired tolerance
+    tol = 1e-8
+    
+
+    
+
+    ################ Benchmark PCGD
+    # create a preconditioner
+    dA = A.toarray()
+    dA = dA[A.getnnz(0) > 0,:]
+    dA = dA[:,A.getnnz(1)>0]
+    db = b[A.getnnz(1) > 0]
+    R = np.sqrt(dA.diagonal())
+    R = scipy.sparse.diags(R).tocsc()
+
+    start_time = time.time()
+    res_arr, x, its = pgd_backsolve(dA.copy(), R, db, tol = tol)
+    finish_time = time.time()
+    time_taken = finish_time - start_time
+
+    
+    err = np.linalg.norm(db - dA.dot(x))
+    pgd_result_str = f"PCGD finished in {its} iterations with residual {err}. It took {time_taken/its} seconds per it."
+    
+    ################ Benchmark ICPCGD
+    # create a preconditioner
+    R = icholesky(dA.copy())
+
+    start_time = time.time()
+    ic_res_arr, x, its = pgd_backsolve(dA, R, db, tol = tol)
+    finish_time = time.time()
+    time_taken = finish_time - start_time
+
+    err = np.linalg.norm(db - dA.dot(x))
+    icpgd_result_str = f"PCGD finished in {its} iterations with residual {err}. It took {time_taken/its} seconds per it."
+
+
+
+    ################ Benchmark Neural PCGD
+    # convert matrix to pytorch sparse format
+    A = torch.sparse_coo_tensor(np.array([A.row, A.col]), A.data, size = [dim_x * dim_y * dim_z, dim_x * dim_y * dim_z])
+    A = A.to(DEVICE, dtype=torch.double)
+    b = torch.from_numpy(b).to(DEVICE, dtype = torch.double).squeeze()
+    start_time = time.time()
+    
+    
+    start_time = time.time()
+    n_res_arr, x, its = neural_pgd(A, b, tol = tol)
+    finish_time = time.time()
+    time_taken = finish_time - start_time
+
+
+    err = torch.linalg.norm(torch.sub(input = b, other = torch.mv(A, x)), 2)
+    npgd_result_str = f"Neural PCGD finished in {its} iterations with residual {err}. It took {time_taken/its} seconds per it."
+
+    print("\n" + pgd_result_str)
+    print(npgd_result_str)
+    print(icpgd_result_str)
+    plt.plot(np.arange(0, len(res_arr)), res_arr, label = "PGD")
+    plt.plot(np.arange(0, len(n_res_arr)), n_res_arr, label = "NPGD")
+    plt.plot(np.arange(0, len(ic_res_arr)), ic_res_arr, label = "ICPGD")
+    plt.title(plot_title)
+    plt.yscale('log')
+    plt.ylabel("Error")
+    plt.xlabel("Iterations")
+    plt.legend()
+    plt.savefig(f"./plots/{save_fn}")
+    plt.show()
---
+++ src/train.py
@@ -0,0 +1,320 @@
+
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import scipy.sparse
+import torchsummary
+
+from src.vector_dataset import *
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torchvision.transforms import ToTensor
+from torch import nn
+
+def init_zeros(m):
+    if isinstance(m, nn.Conv3d):
+        m.weight.data.fill_(0)
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0)
+
+class ResidualBlock(nn.Module):
+    def __init__(self, depth):
+        super(ResidualBlock, self).__init__()
+        self.x_layer = nn.Conv3d(in_channels = depth, out_channels = depth, kernel_size = (3,3,3), padding = "same", bias = False)   # no change
+        self.y_layer = nn.Conv3d(in_channels = depth, out_channels = depth, kernel_size = (3,3,3), padding = "same", bias = False)   # no change
+        self.relu = nn.ReLU(inplace = True)
+        
+        # self.x_layer.apply(init_zeros)
+        # self.y_layer.apply(init_zeros)
+    
+    def forward(self,x):
+        residual = x.clone()
+        x1 = self.relu(self.x_layer(x))
+        x2 = self.y_layer(x1)
+        out = self.relu(x2) + residual
+        return out
+
+
+
+
+class SimpleModel(nn.Module):
+   
+    def __init__(self, dim_x, dim_y, dim_z):
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+        self.dim_z = dim_z
+        super(SimpleModel, self).__init__()
+        depth = 2
+        self.conv1 = nn.Sequential(
+           nn.Conv3d(in_channels = 1, out_channels = depth, kernel_size = (3,3,3), padding = "same"),
+           nn.Conv3d(in_channels = depth, out_channels = depth, kernel_size = (3,3,3), padding = "same"),
+           nn.ReLU(),
+           nn.Conv3d(in_channels = depth, out_channels = depth, kernel_size = (3,3,3), padding = "same"),
+           nn.ReLU(),
+           nn.Conv3d(in_channels = depth, out_channels = depth, kernel_size = (3,3,3), padding = "same"),
+           nn.ReLU(),
+           nn.Conv3d(in_channels = depth, out_channels = depth, kernel_size = (3,3,3), padding = "same"),
+           nn.ReLU(),
+           nn.Conv3d(in_channels = depth, out_channels = depth, kernel_size = (3,3,3), padding = "same"),
+           nn.ReLU(),
+           nn.Conv3d(in_channels = depth, out_channels = depth, kernel_size = (3,3,3), padding = "same"),
+           nn.ReLU(),
+           nn.Conv3d(in_channels = depth, out_channels = depth, kernel_size = (3,3,3), padding = "same"),
+           nn.ReLU())
+        
+
+    
+
+        self.fc_layer = nn.Linear(in_features = 4, out_features = 1)
+        self.out_layer = nn.Conv3d(in_channels = 4, out_channels = 1, kernel_size = (3,3,3), padding = "same")
+
+        self.conv1.apply(init_weights)
+        self.fc_layer.apply(init_weights)
+        
+    # feed through the convolutional layers
+    def forward(self, x):
+        x = self.conv1(x)
+        x = x.permute(0, 4, 2, 3, 1) 
+        x = self.fc_layer(x)
+        x = x.permute(0, 4, 2, 3, 1) 
+        x = x.reshape(x.size(0), 1, x.size(2) * x.size(3) * x.size(4))
+        return x
+
+
+class CNN(nn.Module):
+
+    
+
+    def __init__(self, dim_x, dim_y, dim_z):
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+        self.dim_z = dim_z
+        super(CNN, self).__init__()
+        self.depth = 16
+        self.conv1 = nn.Sequential(
+           nn.Conv3d(in_channels = 1, out_channels = self.depth, kernel_size = (3,3,3), padding = "same"),
+           ResidualBlock(self.depth),
+           ResidualBlock(self.depth))
+        
+
+        self.pool_layer = nn.AvgPool3d(kernel_size = (2,2,2), stride=(2,2,2))
+
+
+        self.conv2 = nn.Sequential(
+           ResidualBlock(self.depth),
+           ResidualBlock(self.depth),
+           ResidualBlock(self.depth),
+           ResidualBlock(self.depth),
+           ResidualBlock(self.depth))
+
+
+        self.upsample_layer = nn.Upsample(scale_factor = (2,2,2))
+        
+        self.conv3 = nn.Sequential(
+           ResidualBlock(self.depth),
+           ResidualBlock(self.depth),
+           ResidualBlock(self.depth))
+
+        self.fc_layer = nn.Linear(in_features = self.depth, out_features = 1)
+        self.out_layer = nn.Conv3d(in_channels = self.depth, out_channels = 1, kernel_size = (1, 1, 1), padding = "same")
+
+            
+    # feed through the convolutional layers
+    def forward(self, x):
+        xr = self.conv1(x).clone()
+        x = self.pool_layer(xr.clone())
+        x = self.conv2(x)
+        x = self.upsample_layer(x) + xr
+        x = self.conv3(x)
+        x = self.out_layer(x)
+    
+        return x
+
+
+def cgd_loss_fn(pred, y):
+    error = 0
+    eps = 1e-24
+    dataset_size = len(pred)
+    for i in range(dataset_size):
+        p = pred[i].squeeze()
+        p = p.flatten()
+        y_ = y[i].squeeze()
+        Af = torch.mv(A, p)
+        fAf = torch.dot(p, Af)
+        bf = torch.dot(p, y_)
+
+        normed_Af = Af/torch.norm(Af)
+        normed_y_ = y_/torch.norm(y_)
+        # e = torch.linalg.norm(
+        #     torch.sub(input = y_, alpha = 1, other = p), ord = 2)
+
+        # error = error + e
+        # alpha = torch.div(bf, fAf).item()
+        # error = error + 1 - torch.square(torch.dot(normed_y_, normed_Af)) 
+        error = error + torch.sum(torch.square(
+            torch.sub(input = y_, alpha = 1, other = Af * bf/(fAf + eps)))) # = ||r - alpha (A*f)||
+    return error / dataset_size
+      
+    
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    """
+    trains model for an epoch
+    returns an array of loss values over the training epoch
+    """
+    loss_array = [] # track losses from each batch
+    model.train() # train mode
+
+    for batch, X in enumerate(dataloader):
+      # move images to device
+      X = X.to(DEVICE, dtype=torch.float64)
+      Y = X
+      if(X.shape[0] == BATCH_SIZE):
+        X = X.view((BATCH_SIZE, 1, dim_x, dim_y, dim_z))
+      else:
+        X = X.view((X.shape[0], 1, dim_x, dim_y, dim_z))
+     
+      # zero gradients from previous step
+      optimizer.zero_grad()
+
+      # compute prediction and loss
+      pred = model(X)
+      loss = loss_fn(pred, Y)
+
+      # backpropogation
+      loss.backward()
+      optimizer.step()
+
+      loss = loss.item()
+      if(batch % 5 == 0):
+        print(f"loss: {loss:>7f}")
+
+      loss_array.append(loss)
+
+
+    return loss_array
+
+
+def test_loop(dataloader, model, loss_fn):
+    """
+    tests your model on the test set
+    returns average loss
+    """
+    test_loss = 0
+    model.eval() # evaluate mode
+
+    size = len(dataloader.dataset)
+
+    with torch.no_grad():
+      for X in dataloader:
+        # move images to GPU
+        X = X.to(DEVICE, dtype=torch.float64)
+        Y = X.clone()
+        X = X.view((1, 1, dim_x, dim_y, dim_z))
+
+        # compute prediction and loss
+        pred = model(X)
+        test_loss += loss_fn(pred, Y).item()
+
+
+    test_loss /= size
+
+    print(f"Test Error:\n Avg loss: {test_loss:>8f} \n")
+
+    return test_loss
+
+
+
+if __name__ == "__main__":
+    # parse --demo flag, if not there FLAGS.demo == False
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--existing", action="store_true", default = False)
+    FLAGS, unparsed = parser.parse_known_args()
+
+    
+
+    dim_x, dim_y, dim_z = 8, 8, 8
+    
+    # make models directory
+    if not os.path.exists("./models/"):
+        os.makedirs("./models/")
+    
+    # define loss function
+    loss_fn = cgd_loss_fn  # use paper defined loss
+    # use double precision
+    torch.set_default_dtype(torch.float64)
+
+    # tweak these constants as you see fit, or get them through 'argparse'
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    BATCH_SIZE = 1200
+    EPOCHS = 5
+
+    domain_name = f"A_matrix_{dim_x}_{dim_y}_{dim_z}_"
+
+    dataset = VectorDataset()
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [0.95, 0.05]) 
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=4, shuffle = True)
+    test_dataloader =  DataLoader(test_dataset, batch_size=1, num_workers=4, shuffle = True)
+    print("Dataset Loaded")
+
+    # load A matrix, its stored as COO
+    A = scipy.sparse.load_npz(f"{domain_name}.npz").toarray()
+    # convert matrix to pytorch sparse format
+    A = torch.tensor(A).to_sparse_csr()
+    # A = torch.sparse_coo_tensor(np.array([A.row, A.col]), A.data).to_sparse_csr()
+    A = A.to(DEVICE, dtype=torch.float64)
+
+    torch.autograd.set_detect_anomaly(True)
+
+    # if(FLAGS.existing):
+    #     models_dir = f"./models/"
+    #     model_fn = "8_8_8_grid.pth"
+    #     model = torch.load(models_dir + model_fn).to(DEVICE)
+    # else:
+    #     model = CNN(dim_x, dim_y, dim_z).to(DEVICE)
+
+    model = CNN(dim_x, dim_y, dim_z).to(DEVICE)
+
+    if(FLAGS.existing):
+        models_dir = f"./models/"
+        model_fn = f"{dim_x}_{dim_y}_{dim_z}_grid_state_final_stg2.pth"
+        model.load_state_dict(torch.load(models_dir + model_fn))
+    
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, amsgrad = True, eps=1e-10)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=1)
+    test_loss, train_loss = [], []
+
+
+
+
+    for t in range(EPOCHS):
+        print(f"Epoch {t+1}\n-------------------------------")
+        print("Optimizer Learning Rate: ", optimizer.param_groups[-1]['lr'])
+
+        epoch_train_loss = train_loop(train_dataloader, model, loss_fn, optimizer)
+        epoch_test_loss = test_loop(test_dataloader, model, loss_fn)
+
+        train_loss = train_loss + epoch_train_loss
+        test_loss = test_loss + [epoch_test_loss]
+        scheduler.step()
+
+
+    # save the model
+    torch.save(model.state_dict(), f"./models/{dim_x}_{dim_y}_{dim_z}_grid_state_final_stg2.pth")
+    fig, axs = plt.subplots(1, 2, figsize=(15, 15))
+    axs[0].plot(np.arange(1, len(train_loss) + 1), train_loss)
+    axs[0].set_title("Train Loss")
+    axs[0].set_xlabel("Iteration")
+    axs[0].set_ylabel("Loss")
+
+    axs[1].plot(np.arange(1, len(test_loss) + 1),  test_loss)
+    axs[1].set_title("Validation Loss")
+    axs[1].set_xlabel("Epoch")
+    axs[1].set_ylabel("Average Loss")
+    plt.show()
+    fig.savefig("./test_train_loss.jpg")
